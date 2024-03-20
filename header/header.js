9206221a0a32a39eeb208a45d7b59edb12df1b56
Iftekhar Sakib <pro.iftekhar@gmail.com>
2024-03-20 14:00:55 +0600

    .

---
+++ header/header.js
@@ -0,0 +1,15 @@
+
+fetch('/header/header.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(html => {
+        document.getElementById('header').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Error fetching header:', error);
+        // Handle the error here, e.g., display an error message to the user
+    });
--- nav.js
+++
@@ -1,9 +0,0 @@
-fetch('nav.html')
-.then(response => response.text())
-.then(html => {
-  document.getElementById('nav').innerHTML = html;
-  
-});
-
-
-
